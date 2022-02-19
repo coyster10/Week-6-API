2df9e792116be0990af0aa16896f100eb837cafe
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2022-02-19 13:00:07 -0600

    Wrote code for PunkAPI Data

--- Week-6-api/Week-6-api/Controllers/group2_api.cs
+++ Week-6-api/Week-6-api/Controllers/group2_api.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Week_6_api.Controllers
 {
@@ -6,26 +7,47 @@ namespace Week_6_api.Controllers
     [Route("[controller]")]
     public class group2_api : ControllerBase
     {
-        [HttpGet(Name = "group2_api")]
+        [HttpGet(Name = "getBeerData")]
+        public ActionResult<List<PunkAPI>> GetBeerData()
+        {
+            HttpClient client = new HttpClient();
+            string result;
+            
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("https://api.punkapi.com/v2/beers").Result;
+                response.EnsureSuccessStatusCode();
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
+            List<PunkAPI> results = JsonConvert.DeserializeObject<List<PunkAPI>>(result);
+
+            return Ok(results);
+        }
+
+    }
+    public class DataUSA
+    {
+        public string IdNation { get; set; }
+        public string Nation { get; set; }
+        public int IdYear { get; set; }
+        public int Year { get; set; }
+        public int Population { get; set; }
+        public string SlugNation { get; set; }
+    }
+
+    public class PunkAPI
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TagLine { get; set; }
+        public DateTime FirstBrewed { get; set; }
+        public string Description { get; set; }
     }
 }
 
-public class DataUSA
-{
-    public string idNation { get; set; }
-    public string nation { get; set; }
-    public int idYear { get; set; }
-    public int year { get; set; }
-    public int population { get; set; }
-    public string slugNation { get; set; }
-}
 
-public class PunkAPI
-{
-    public int id { get; set; }
-    public string name { get; set; }
-    public string tagLine { get; set; }
-    public DateOnly firstBrewed { get; set; }
-    public string description { get; set; }
-}
