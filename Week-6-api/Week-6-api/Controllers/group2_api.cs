0c0fdb4c10e28812635d4cb6a26876f432d34494
coyster10 <id6145ic@go.minnstate.edu>
2022-02-19 15:49:08 -0600

    DataUSA JSON object return created

--- Week-6-api/Week-6-api/Controllers/group2_api.cs
+++ Week-6-api/Week-6-api/Controllers/group2_api.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace Week_6_api.Controllers
 {
@@ -6,26 +9,75 @@ namespace Week_6_api.Controllers
     [Route("[controller]")]
     public class group2_api : ControllerBase
     {
-        [HttpGet(Name = "group2_api")]
+        [HttpGet(Name = "Data_From_USA")]
+        public ActionResult<DataUSA> Get()
+        {
+            HttpClient client = new HttpClient();
+            dynamic? obj = new ExpandoObject();
+
+            string result;
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("https://datausa.io/api/data?drilldowns=Nation&measures=Population").Result;
+                response.EnsureSuccessStatusCode();
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            var list = JsonConvert.DeserializeObject<DataUSA>(result);
+            return Ok(list);
+        }
+
+        /*public ActionResult<List<PunkAPI>> Get()
+        {
+            HttpClient client = new HttpClient();
+            dynamic? obj = new ExpandoObject();
+
+            string result;
+
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("https://api.punkapi.com/v2/beers").Result;
+                response.EnsureSuccessStatusCode();
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            var list = JsonConvert.DeserializeObject<List<PunkAPI>>(result);
+            return Ok(list);
+        }*/
 
     }
-}
 
-public class DataUSA
-{
-    public string idNation { get; set; }
-    public string nation { get; set; }
-    public int idYear { get; set; }
-    public int year { get; set; }
-    public int population { get; set; }
-    public string slugNation { get; set; }
-}
+    public class DataUSA
+    {
+        List<data> contents { get; set; }
+    }
+    public class data
+    {
+        public string IdNation { get; set; }
+        public string Nation { get; set; }
+        public int IdYear { get; set; }
+        public string Year { get; set; }
+        public int Population { get; set; }
+        public string SlugNation { get; set; }
+    }
 
-public class PunkAPI
-{
-    public int id { get; set; }
-    public string name { get; set; }
-    public string tagLine { get; set; }
-    public DateOnly firstBrewed { get; set; }
-    public string description { get; set; }
+    public class PunkAPI
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string tagLine { get; set; }
+        public DateOnly firstBrewed { get; set; }
+        public string description { get; set; }
+    }
 }
